24e6c8f79e47cbe63a4279aa67d7e092dad30558
Stefan Nieke <info@niekes.com>
2017-01-13 12:32:58 +0100

    add slide effect on route change

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
 	<div class="row">
-		<div class="twelve columns" data-ui-view></div>
+		<div class="twelve columns slide" data-ui-view></div>
 	</div>
 <?php get_footer(); ?>
--- src/js/controllers/detailCtrl.js
+++ src/js/controllers/detailCtrl.js
@@ -9,13 +9,19 @@ app.controller('detailCtrl', function($rootScope, $http, $stateParams, $q, users
 	$http.get(appInfo.apiUrl + 'posts?filter[name]=' + $stateParams.slug).then(function successCallback(res){
 		$detailCtrl.post = res.data[0];
 		var requests = [
-			$http.get(appInfo.apiUrl + 'editlink?post=' + $detailCtrl.post.id, { headers: {'X-WP-Nonce': appInfo.nonce} }),
 			users.get({id: $detailCtrl.post.author})
 		];
+		// @todo get editlink later
 		$q.all(requests).then(function successCallback(res){
-			$detailCtrl.post.editPostLink = res[0].data.editPostLink;
 			$detailCtrl.author = res[1];
 			$rootScope.isLoading = false;
+		}).then(function(){
+			$http.get(
+				appInfo.apiUrl + 'editlink?post=' +
+				$detailCtrl.post.id,
+				{ headers: {'X-WP-Nonce': appInfo.nonce} }).then(function successCallback(res){
+					$detailCtrl.post.editPostLink = res.data.editPostLink;
+				});
 		});
 	});
 });
