4a0bf451a4fc4a19c0eac4150b111f54d7958e00
Stefan Nieke <info@niekes.com>
2017-01-13 22:25:15 +0100

    remove fading effect

--- src/js/controllers/listCtrl.js
+++ src/js/controllers/listCtrl.js
@@ -2,14 +2,11 @@
 
 app.controller('listCtrl', function($rootScope, $state, posts) {
 
-	$rootScope.isLoading = true;
-
 	var $listCtrl = this;
 
 	$listCtrl.pageTitle = 'Blog Listing Page';
 
 	posts.query(function(res) {
 		$listCtrl.posts = res;
-		$rootScope.isLoading = false;
 	});
 });
